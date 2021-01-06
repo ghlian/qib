dbce8e411aee0295ea6917706b409155100e7727
ghlian <ligu151@163.com>
2021-01-06 20:45:19 +0800

    correct the bash file to the right path

--- app/debug.sh
+++ app/debug.sh
@@ -2,5 +2,6 @@
 # DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 # rlwrap ${QBIN} ${DIR}/debug.q -appdir ${DIR} -p 5000
 
-DIR=/home/ghlian/CODE_LIAN/code_kdb/QInteractiveBrokers/app
+# DIR=/home/ghlian/CODE_LIAN/code_kdb/QInteractiveBrokers/app
+DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 q ${DIR}/debug.q -appdir ${DIR} -p 5000
